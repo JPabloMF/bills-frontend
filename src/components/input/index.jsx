f5bddf0cf11dc8583d2a0bcd669ec7acfbd824b6
JpabloMF <juanpablo45671@gmail.com>
2020-01-25 21:17:19 -0500

    input component added

---
+++ src/components/input/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Input as InputComponent } from 'semantic-ui-react';
+
+const Input = ({
+  placeholder,
+  onChange,
+  disabled,
+  value,
+  title,
+  error,
+  name,
+  icon,
+  id
+}) => {
+  return (
+    <div>
+      {title && <label htmlFor={id}>{title}</label>}
+      <InputComponent
+        placeholder={placeholder}
+        onChange={onChange}
+        disabled={disabled}
+        value={value}
+        error={error}
+        name={name}
+        icon={icon}
+      />
+    </div>
+  );
+};
+
+Input.defaultProps = {
+  placeholder: '',
+  disabled: false,
+  error: false,
+  icon: ''
+};
+
+Input.propTypes = {
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+  value: PropTypes.string.isRequired,
+  error: PropTypes.bool,
+  name: PropTypes.string.isRequired,
+  icon: PropTypes.string
+};
+
+export default Input;
